87cd1e7756c7138c1821a0e5f007ece4cc3c846f
Tom Hickey <thickey@gmail.com>
2012-03-08 13:59:22 -0500

    updatedproject to 0.1.1-SNAPSHOT. added :min-lein-version

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject lein-nevam "0.1.0"
+(defproject lein-nevam "0.1.1-SNAPSHOT"
   :description "Converts Maven pom.xml files to project.clj files."
   :url "https://github.com/thickey/lein-nevam"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[org.clojure/data.zip "0.1.0"]])
+  :dependencies [[org.clojure/data.zip "0.1.0"]]
+  :min-lein-version "2.0.0")
